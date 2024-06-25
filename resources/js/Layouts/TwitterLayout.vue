b908ec2055856477ef852c382de76ab6387cc75c
atroopa <atroopa@gmail.com>
2024-06-25 22:46:15 +0330

    work on center tweet bar

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -14,24 +14,26 @@ import Emoticon from 'vue-material-design-icons/Emoticon.vue';
 import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue';
 import MenuItem from '@/Components/MenuItem.vue';
 
+let createTweet = ref(false);
+let tweet = ref('')
 let randImg2 = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}/100`);
 
 </script>
 
 <template>
-    <div class="fixed w-full">
-        <div class="max-w-[1400px] flex mx-auto">
-            <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:px-4 lg:mx-auto">
-                <div class="p-2 px-3 mb-4">
-                    <Twitter fillColor="#FFFFFF" :size="37" />
-                </div>
-                <MenuItem iconString="خانه" />
-                <MenuItem iconString="کاوش" />
-                <MenuItem iconString="اعلان‌ها" />
-                <MenuItem iconString="پیام‌ها" />
-                <MenuItem iconString="پروفایل" />
+  <div class="fixed w-full">
+    <div class="max-w-[1400px] flex mx-auto">
+      <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:px-4 lg:mx-auto">
+        <div class="p-2 px-3 mb-4">
+          <Twitter fillColor="#FFFFFF" :size="37" />
+        </div>
+        <MenuItem iconString="خانه" />
+        <MenuItem iconString="کاوش" />
+        <MenuItem iconString="اعلان‌ها" />
+        <MenuItem iconString="پیام‌ها" />
+        <MenuItem iconString="پروفایل" />
 
-                <button class="
+        <button class="
                     lg:w-full
                     mt-8
                     ml-2
@@ -44,22 +46,22 @@ let randImg2 = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}
                     rounded-full
                     cursor-pointer
         ">
-                    <span class="lg:block hidden">بپرس</span>
-                    <span class="block lg:hidden">
-                        <Feather />
-                    </span>
+          <span class="lg:block hidden">بپرس</span>
+          <span class="block lg:hidden">
+            <Feather />
+          </span>
 
-                </button>
+        </button>
+      </div>
+
+      <div class="lg:w-7/12 w-11/12 border-x border-gray-800 relative">
+        <div class="bg-black bg-opacity-50 backdrop-blur-md z-10 absolute w-full ">
+          <div class="border-gray-800 border-b w-full">
+            <div class="w-full text-white text-[22px] font-extrabold p-4">
+              خانه
             </div>
-
-            <div class="lg:w-7/12 w-11/12 border-x border-gray-800 relative">
-                <div class="bg-black bg-opacity-50 backdrop-blur-md z-10 absolute w-full ">
-                    <div class="border-gray-800 border-b w-full">
-                        <div class="w-full text-white text-[22px] font-extrabold p-4">
-                            خانه
-                        </div>
-                        <div class="flex">
-                            <div class="
+            <div class="flex">
+              <div class="
                                     flex 
                                     items-center 
                                     justify-center 
@@ -76,11 +78,11 @@ let randImg2 = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}
                                     duration-200
                                     ease-in-out
                                     ">
-                                <div class="inline-block text-center border-b-4 border-b-[#1C9CEF] h-[60px]">
-                                    <div class="my-auto mt-4">برای تو</div>
-                                </div>
-                            </div>
-                            <div class="
+                <div class="inline-block text-center border-b-4 border-b-[#1C9CEF] h-[60px]">
+                  <div class="my-auto mt-4">برای تو</div>
+                </div>
+              </div>
+              <div class="
                                     w-full 
                                     h-[60px] 
                                     text-gray-500
@@ -94,24 +96,23 @@ let randImg2 = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}
                                     duration-200
                                     ease-in-out
                                     ">
-                                    <div class="text-center">وکلا</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <div class="text-center">وکلا</div>
+              </div>
             </div>
-            <div class="absolute top-0 z-0 h-full overflow-auto scrollbar-hide">
-                <div class="mt-[126px] ">
-                  <slot />
-                  <div class="pb-4"></div>
-                </div>
-            </div>
+          </div>
+        </div>
+      </div>
+      <div class="absolute top-0 z-0 h-full overflow-auto scrollbar-hide">
+        <div class="mt-[126px] ">
+          <slot />
+          <div class="pb-4"></div>
+        </div>
+      </div>
 
-            <div class="lg:block hidden lg:w-4/12 h-screen border-gray-800 pl-4">
-              <div class="w-full p-1 mt-2 px-4 lg:flex items-center rounded-full hidden bg-[#212327]">
-                <Magnify fillColor="#5e5c5c" :size="25" />
-                <input
-                  class="
+      <div class="lg:block hidden lg:w-4/12 h-screen border-gray-800 pl-4">
+        <div class="w-full p-1 mt-2 px-4 lg:flex items-center rounded-full hidden bg-[#212327]">
+          <Magnify fillColor="#5e5c5c" :size="25" />
+          <input class="
                     appearance-none
                     w-full
                     border-0
@@ -122,69 +123,93 @@ let randImg2 = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}
                     leading-tight
                     focus:ring-0
                     text-right
-                  "
-                  type="text"
-                  placeholder="جستوجوی وکیل"
-                >
-              </div> 
-                <div class="w-full mt-4 rounded-lg lg:block hidden bg-[#212327]">
-                  <div class="text-right w-full p-4 text-white font-extrabold mb-6 text-[20px]">
-                      چه اتفاقی در حال رخداد است
-                  </div>
-                  <div class="h-[80px] hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
-                    <div class="flex p-3 justify-between h-[80px] py-3">
-                      <div>
-                        <div class="text-[14px] text-gray-400">آخرین اخبار حقوقی</div>
-                        <div class="w-full text-white font-extrabold mb-6 text-[17px]">نمونه آزمون حقوق امسال</div>
-                      </div>
-                      <img class="rounded-xl" :src="randImg2" alt="" />
-                    </div>
-                  </div>
-
-                  <div class=" hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
-                    <div class="flex p-3 justify-between">
-                      <div>
-                        <div class="text-[14px] text-gray-400">بیشترین پاسخ داده شده </div>
-                        <div class="w-full text-white font-extrabold text-[17px]">چگونه یک شکایت تنظیم کنم؟</div>
-                        <div class="text-[14px] text-gray-400 "> 5,923 عدد سوال</div>
-                      </div>
-                      <DotsHorizontal fillColor="#5e5c5c" />
-                    </div>
-                  </div>
-
-                  <div class=" hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
-                    <div class="flex p-3 justify-between">
-                      <div>
-                        <div class="text-[14px] text-gray-400"> بیشترین اشتراگذاری سوال</div>
-                        <div class="w-full text-white font-extrabold text-[17px]">هزینه مشاوره حقوقی چقدر است؟</div>
-                        <div class="text-[14px] text-gray-400 "> 98,454 عدد سوال</div>
-                      </div>
-                      <DotsHorizontal fillColor="#5e5c5c" />
-                    </div>
-                  </div>
-
-                  <div class=" hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
-                    <div class="flex p-3 justify-between">
-                      <div>
-                        <div class="text-[14px] text-gray-400">بیشترین پسند کاربران</div>
-                        <div class="w-full text-white font-extrabold text-[17px]">تفاوت قانون مدنی و قانون کیفری چیست؟</div>
-                        <div class="text-[14px] text-gray-400 "> 12,432 عدد سوال</div>
-                      </div>
-                      <DotsHorizontal fillColor="#5e5c5c" />
-                    </div>
-                  </div>
-
-                </div>         
-            </div>
+                  " type="text" placeholder="جستوجوی وکیل">
         </div>
+        <div class="w-full mt-4 rounded-lg lg:block hidden bg-[#212327]">
+          <div class="text-right w-full p-4 text-white font-extrabold mb-6 text-[20px]">
+            چه اتفاقی در حال رخداد است
+          </div>
+          <div class="h-[80px] hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+            <div class="flex p-3 justify-between h-[80px] py-3">
+              <div>
+                <div class="text-[14px] text-gray-400">آخرین اخبار حقوقی</div>
+                <div class="w-full text-white font-extrabold mb-6 text-[17px]">نمونه آزمون حقوق امسال</div>
+              </div>
+              <img class="rounded-xl" :src="randImg2" alt="" />
+            </div>
+          </div>
+
+          <div class=" hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+            <div class="flex p-3 justify-between">
+              <div>
+                <div class="text-[14px] text-gray-400">بیشترین پاسخ داده شده </div>
+                <div class="w-full text-white font-extrabold text-[17px]">چگونه یک شکایت تنظیم کنم؟</div>
+                <div class="text-[14px] text-gray-400 "> 5,923 عدد سوال</div>
+              </div>
+              <DotsHorizontal fillColor="#5e5c5c" />
+            </div>
+          </div>
+
+          <div class=" hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+            <div class="flex p-3 justify-between">
+              <div>
+                <div class="text-[14px] text-gray-400"> بیشترین اشتراگذاری سوال</div>
+                <div class="w-full text-white font-extrabold text-[17px]">هزینه مشاوره حقوقی چقدر است؟</div>
+                <div class="text-[14px] text-gray-400 "> 98,454 عدد سوال</div>
+              </div>
+              <DotsHorizontal fillColor="#5e5c5c" />
+            </div>
+          </div>
+
+          <div class=" hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+            <div class="flex p-3 justify-between">
+              <div>
+                <div class="text-[14px] text-gray-400">بیشترین پسند کاربران</div>
+                <div class="w-full text-white font-extrabold text-[17px]">تفاوت قانون مدنی و قانون کیفری چیست؟</div>
+                <div class="text-[14px] text-gray-400 "> 12,432 عدد سوال</div>
+              </div>
+              <DotsHorizontal fillColor="#5e5c5c" />
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
-    <div id="OverlaySection" class="fixed top-0 left-0 w-full h-screen bg-black md:bg-gray-400 md:bg-opacity-30 md:p-3">
+  </div>
+  <div id="OverlaySection" class="fixed top-0 left-0 w-full h-screen bg-black md:bg-gray-400 md:bg-opacity-30 md:p-3">
+    <div class="md:max-w-2xl md:max-auto md:mt-10 md:rounded-xl bg-black">
+      <div class="
+                flex
+                items-center
+                justify-center
+                md:inline-block
+                p-2
+                m-2
+                rounded-full
+                cursor-pointer
+        ">
+        <div class="hover:bg-gray-800 inline-block p-2 rounded-full cursor-pointer">
+          <Close fillColor="#FFFFFF" :size="28" class="md:block hidden" />
+          <ArrowLeft fillColor="#FFFFFF" :size="28" class="md:hidden block" />
+        </div>
+        <button
+          :disabled="!tweet"
+          :class="tweet ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400' "
+          class="md:hidden font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer">
+          بپرس
+        </button>
+      </div>
+
+      <div>
+        
+      </div>
 
     </div>
+  </div>
 </template>
 
 <style>
 body {
-    background-color: black;
+  background-color: black;
 }
 </style>
