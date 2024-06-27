80e8b24442304f48548e24563d0e6f41cdc7931c
atroopa <atroopa@gmail.com>
2024-06-27 19:05:23 +0330

    show tweet test is succed

--- resources/js/Components/Tweet.vue
+++ resources/js/Components/Tweet.vue
@@ -9,14 +9,40 @@ import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
 import TrashCanOutline from 'vue-material-design-icons/TrashCanOutline.vue';
 
 const props = defineProps({
-  tweet: Object
+  tweet: {
+    type: Object,
+    required: true
+  }
 });
 
 let openOptions = ref(false);
 </script>
 
 <template>
-  <div class="min-w-[60px]">
+  <div class="min-w-[60px] ">
     <img class="rounded-full m-2 mt-3" width="50" :src="props.tweet.image">
+  </div>
+  <div class="p-2 w-full">
+    <div class="font-extrabold flex items-center justify-between mt-0.5 mb-1.5">
+      <div class="flex items-center">
+        <div>{{ props.tweet.name }}</div>
+        <span class="font=[300] text-[14px] text-gray-500 pl-2">{{ props.tweet.handle }}</span>
+      </div>
+      <div class="hover:bg-gray-800 rounded-full cursor-pointer relative">
+        <button type="button" class="block p-2" @click="openOptions = !openOptions">
+          <DotsHorizontal />
+        </button>
+      </div>
+    </div>
+
+    <div class="p-3 text-right">{{ props.tweet.tweet }}</div>
+    <div v-if="props.tweet.file">
+      <div v-if="!props.tweet.is_video" class="rounded-xl w-full">
+        <img :src="props.tweet.file" class="mt-2 object-fill rounded-xl w-full">
+      </div>
+      <div v-else>
+        <video class="rounded-xl" :src="props.tweet.file" controls></video>
+      </div>
+    </div>
   </div>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,30 +1,29 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
-import Tweet from '@/Components/Tweet.vue'
+import Tweet from '@/Components/Tweet.vue';
 import TwitterLayout from '@/Layouts/TwitterLayout.vue';
 </script>
 
 <template>
-    <Head title="Welcome" />
+  <Head title="Welcome" />
 
-    <TwitterLayout>
-        <div class="text-white">
-            <div class="flex">
-                <Tweet :tweet="{
-                    name: 'omid hajavi',
-                    handle: '@omidhajavi',
-                    image: 'https://randomuser.me/api/portraits/men/61.jpg',
-                    tweet:'اگر شاکی در حضور قاضی به متهم حمله کند جرم آن چیست ؟ ',
-                    file: '/videos/attck.mp4',
-                    is_video: true,
-                    comments: '35',
-                    retweets: '54',
-                    likes: '88',
-                    analytics: '81'
-                }"/>
-            </div>
-            <div class="border-b border-b-gray-800 mt-2"></div>
-        </div>
-    </TwitterLayout>
-
+  <TwitterLayout>
+    <div class="text-white">
+      <div class="flex">
+        <Tweet :tweet="{
+          name: 'omid hajavi',
+          handle: '@omidhajavi',
+          image: 'https://randomuser.me/api/portraits/men/61.jpg',
+          tweet: 'اگر شاکی در حضور قاضی به متهم حمله کند جرم آن چیست ؟',
+          file: '/videos/attack.mp4',
+          is_video: true,
+          comments: '35',
+          retweets: '54',
+          likes: '88',
+          analytics: '81'
+        }"/>
+      </div>
+      <div class="border-b border-b-gray-800 mt-2"></div>
+    </div>
+  </TwitterLayout>
 </template>
