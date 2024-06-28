d952864f4a9195fd7f80123d4c644928c43f19a1
atroopa <atroopa@gmail.com>
2024-06-28 15:46:05 +0330

    fixed bug

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Inertia\Inertia;
 
 Route::get('/', [App\Http\Controllers\TweetController::class, 'index'])->name('tweets.index');
 Route::Post('/tweets', [App\Http\Controllers\TweetController::class, 'store'])->name('tweets.store');
-Route::delete('/tweets/{id}', [App\Http\Controllers\TweetController::class, 'destory'])->name('tweets.destory');
+Route::delete('/tweets/{id}', [App\Http\Controllers\TweetController::class, 'destroy'])->name('tweets.destroy');
 
 
 require __DIR__.'/auth.php';
